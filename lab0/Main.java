06d19245bd13c5d7e079485f0cff0c192c3af9ec
Rithwik-007 <2405748@kiit.ac.in>
2025-12-18 16:36:12 +0530

    .

--- lab0/Main.java
+++ lab0/Main.java
@@ -4,6 +4,7 @@ package lab0;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String args[]) {
+
         System.out.println("Hello Rithwik");
     }
 }
